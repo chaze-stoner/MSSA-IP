b442a0efb9ee7a9d58fcb1e64a4606c07c8a1a51
chaze-stoner <chaze-stoner@users.noreply.github.com>
2020-04-26 23:54:24 -0400

    Recreated the application with a new Identity. It just works baby!

---
+++ MSSAInformationPortal/Areas/Identity/Data/AuthDBContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using MSSAInformationPortal.Areas.Identity.Data;
+
+namespace MSSAInformationPortal.Data
+{
+    public class AuthDBContext : IdentityDbContext<MSSAInformationPortalUser>
+    {
+        public AuthDBContext(DbContextOptions<AuthDBContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            // Customize the ASP.NET Identity model and override the defaults if needed.
+            // For example, you can rename the ASP.NET Identity table names and more.
+            // Add your customizations after calling base.OnModelCreating(builder);
+        }
+    }
+}
---
+++ MSSAInformationPortal/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MSSAInformationPortal.Areas.Identity.Data;
+using MSSAInformationPortal.Data;
+
+[assembly: HostingStartup(typeof(MSSAInformationPortal.Areas.Identity.IdentityHostingStartup))]
+namespace MSSAInformationPortal.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<AuthDBContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("AuthDBContextConnection")));
+
+                services.AddDefaultIdentity<MSSAInformationPortalUser>(options => options.SignIn.RequireConfirmedAccount = false)
+                    .AddEntityFrameworkStores<AuthDBContext>();
+            });
+        }
+    }
+}
